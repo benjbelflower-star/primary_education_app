591a4ef3157cdba5df913a42054d2b7c3a57e5e4
Ben Belflower <benjbelflower@gmail.com>
2026-03-10 12:49:54 -0700

    Report file one-level too low. Corrected ../ coding

--- app/reports/tutor-compliance/page.tsx
+++ app/reports/tutor-compliance/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import RoleGuard from "../../../../components/RoleGuard";
+import RoleGuard from "../../../components/RoleGuard";
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
